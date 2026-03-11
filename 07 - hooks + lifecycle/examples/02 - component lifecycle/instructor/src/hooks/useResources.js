5cb8887b9eab098a9213bd89d41ce173e01de146
oaao <oliver+github@abolish.golf>
2026-03-11 16:40:27 -0600

    rev [07 - hooks]: ex02 - code skeleton for our useResources hook

--- 07 - hooks + lifecycle/examples/02 - component lifecycle/instructor/src/hooks/useResources.js
+++ 07 - hooks + lifecycle/examples/02 - component lifecycle/instructor/src/hooks/useResources.js
@@ -2,11 +2,32 @@ import { useEffect, useState } from 'react';
 
 
 export function useResources() {
-    // 1. I'm going to have state for: resources (holds fetched data), loading state, error msg
+    // 1. state
+    const [resources, setResources] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);  // so we can conditionally render e.g. loading msg
+    const [error, setError]         = useState(null);  // so we can conditionally render error msgs
 
-    // 2. I'm going to have an async function for fetching data from API (normally in its own layer)
+    // 2. fetch function
+    async function fetchResources() {
+        
+    }
 
-    // 3. We're going to have an effect that fetches resources on page load
+    // 3. effect
+    useEffect(
+        // param 1: the callback
+        () => {
+            fetchResources();
+        },
+        // param 2: dependency array
+        []  // -> only fire effect when page loads
+    )
 
-    // 4. We'll also have a refetch function
+    // 4. function to refresh data
+    function refetch() {
+        // from this hook, I also want to return a way to refresh resources
+        fetchResources();
+    }
+
+    // I'm returning an overall object from this hook that contains:
+    return { resources, isLoading, error, refetch }
 }
